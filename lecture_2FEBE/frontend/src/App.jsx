ae64a5b084b978d8c45cb58afac48919e77f6869
The-Parthib <rijupanja81@gmail.com>
2025-04-07 00:08:34 +0530

    basic comit volume 1

---
+++ lecture_2FEBE/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <>
+      <h1>Parthib aur Full Stack</h1>
+
+      <p>JOKES: {jokes.length}</p>
+      {jokes.map((joke) => {
+        return (<div key={jokes.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>)
+      })}
+    </>
+  );
+}
+
+export default App;

2d24ecde814dd32ffbfe9f556c353571f19d2d62
The-Parthib <rijupanja81@gmail.com>
2025-05-06 23:35:32 +0530

    approach 1 : Using IIFE to connect MongoDB

--- projecct_1/src/index.js
+++ projecct_1/src/index.js
@@ -1,13 +1,38 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants';
 
+
+
+
+
+
+
+
+
+
+/*
+// Approach No 1: Using IIFE (Immediately Invoked Function Expression) to connect to MongoDB and start the server
+import express from 'express';
+const app = express();
+
 // IFE immediate function execute
 
 ( async () => { 
     try{
        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,)
+       app.on('error', (err) => {
+        console.error("Error: ", err);
+        throw err;
+       });
+
+       app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+       });
     }
     catch(error){
         console.error("ERROR: ", error);
+        throw error;
     }
  })()
+
+*/

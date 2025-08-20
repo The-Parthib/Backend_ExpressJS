f56e984ffb297ec9888078f6adae9f4ca23becef
The-Parthib <rijupanja81@gmail.com>
2025-08-21 00:21:01 +0530

    feat: enhance server functionality with URL parsing and new routes; update dependencies

--- lecture_1/index2.js
+++ lecture_1/index2.js
@@ -1,26 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 // const server = http.createServer((req,res)=>{
 //     console.log(req.headers);
 //     res.end("Hello from server");
 // })
-const server = http.createServer((req,res)=>{
-    const log = `${new Date().toISOString()} : ${req.url} New Request\n`
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch (req.url) {
-            case "/":
-                res.end("HomePage")
-                break;
-            case "/about":
-                res.end("I'm Parthib Panja");
-                break;
-        
-            default:
-                res.end("404 Page Note found")
-                break;
-        }
-    })
-})
+const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${new Date().toISOString()} : ${req.url} New Request\n`;
+  const myUrl = url.parse(req.url,true);
+  console.log(myUrl)
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const user = myUrl.query.name
+        res.end(`Hii, ${user}`);
+        break;
 
-server.listen(2004,"localhost",()=>console.log("Server Started..!!"))
+      case "/search":
+        const serach = myUrl.query.search_q
+        res.end("Here is the result for "+serach)
+        break;
+
+      default:
+        res.end("404 Page Note found");
+        break;
+    }
+  });
+});
+
+server.listen(2004, "localhost", () => console.log("Server Started..!!"));

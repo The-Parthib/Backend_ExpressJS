d5b69c9477c76811fd0aaf71b90727b418a12945
The-Parthib <rijupanja81@gmail.com>
2025-11-09 00:48:05 +0530

    feat: update user model with password hashing, add refresh token field, and create video model with pagination support

--- projecct_1/src/models/user.model.js
+++ projecct_1/src/models/user.model.js
@@ -1,45 +1,62 @@
-import mongoose, {Schema, Types} from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; // used for hashing password
 
-const userSchema = new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        index:true // optimizes searching -> list like DBMS searching
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // optimizes searching -> list like DBMS searching
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:{
-        type:String,
-        required:true,
-        trim:true,
-        index:true
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar:{
-        type:String, // cloudinary url
-        required:true,
-
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
     },
-    coverImage:{
-        type:String, // cloudinary url
+    coverImage: {
+      type: String, // cloudinary url
     },
-    watchHostory:[
-        {
-            type:Schema.Objects.OnjectId,
-            ref:'Video'
-        }
+    watchHistory: [
+      {
+        type: Schema.Types.OnjectId,
+        ref: "Video",
+      },
     ],
-    password:{
-        types:String,
+    password: {
+      type: String,
+      required: [true, "Password is required"], // custom error message
+    },
+    refreshTocken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-    }
-})
+// mongoose middleware -> to hash password before saving user document
+userSchema.pre("save", function (next) {
+  //just to make sure it will execute only when passwprd has been changed or is new
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10); // 10 -> salt rounds, higher the rounds more secure but slower
+  next();
+});
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
---
+++ projecct_1/src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile:{
+        type: String,  // cloudanyary
+        required: true
+
+    },
+    thumbnail:{
+        type: String,  // cloudanyary
+        required: true
+
+    },
+    title:{
+        type: String,  // need to put by user
+        required: true
+    },
+    description:{
+        type: String,  // need to put by user
+        required: true
+    },
+    duration:{
+        type: Number,  // need to put by user
+        required: true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,  // need to put by user
+        default: true,
+        required: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
+
+},{timestamps:true}); // for createdAt and updatedAt
+
+videoSchema.plugin(mongooseAggregatePaginate); // mongoose middleware -> to add pagination feature to aggregate queries
+
+export const Video = mongoose.model('Video', videoSchema);

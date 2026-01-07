c5e630a77756682002788b6a857fa613e96897bf
The-Parthib <rijupanja81@gmail.com>
2026-01-07 23:12:08 +0530

    Add response handling to getWatchHistory controller: return user watch history with success message

--- projecct_1/src/controllers/user.controller.js
+++ projecct_1/src/controllers/user.controller.js
@@ -511,6 +511,10 @@ const getWatchHistory = asyncHandler(async (req, res) => {
       }
     }
   ])
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user[0]?.watchHistory , "User watch history fetched successfully"));
 });
 
 export {

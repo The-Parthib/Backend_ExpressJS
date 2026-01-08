3a2b11728a853a0c4afdbc2d168bf2cdb8b34d21
The-Parthib <rijupanja81@gmail.com>
2026-01-08 23:11:17 +0530

    Refactor user.routes.js: add missing route handlers for user account updates, avatar and cover image uploads, channel profile, and watch history

--- projecct_1/src/routes/user.routes.js
+++ projecct_1/src/routes/user.routes.js
@@ -6,6 +6,11 @@ import {
   refreshAccessToken,
   changeCurrentPassword,
   getCurrentUser,
+  updateUserAccount,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,5 +36,13 @@ router.route("/logout").post(verifyJWT, logOutUser); // added a middleware to ve
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch( verifyJWT, updateUserAccount);
+// ----------------- Update User Avatar/Cover Image Route ---------------------------------------------
+router.route("/update-avatar").patch( verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-cover").patch( verifyJWT, upload.single("coverImage", updateUserCoverImage));
+// ----------------------------------------------------------------------------------------------------
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;

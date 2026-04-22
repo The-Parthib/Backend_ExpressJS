52f574838c6a3153b84cde1a15e87aba1b1ec699
The-Parthib <rijupanja81@gmail.com>
2026-04-22 23:38:38 +0530

    feat: initialize project structure with express server configuration

---
+++ project_2/server.js
@@ -0,0 +1,11 @@
+// USED to start the server
+
+import app from "./src/app.js";
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
---
+++ project_2/src/app.js
@@ -0,0 +1,7 @@
+// The main purpose is to create the Server here
+
+import express from "express";
+
+const app = express();
+
+export default app; 

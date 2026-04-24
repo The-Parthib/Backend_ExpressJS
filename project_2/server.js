62dc1688798aa48e3b2e627de17c16c92e0b6f93
The-Parthib <rijupanja81@gmail.com>
2026-04-25 00:23:43 +0530

    feat: initialize Express application with basic CRUD note endpoints and server entry point

--- project_2/server.js
+++ project_2/server.js
@@ -5,7 +5,4 @@ const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-
-
-
+});     
--- project_2/src/app.js
+++ project_2/src/app.js
@@ -4,4 +4,43 @@ import express from "express";
 
 const app = express();
 
+// Middleware to parse JSON data from the request body 
+app.use(express.json());
+
+// NOTES to be added here 
+const notes = []
+
+// APIs
+app.post("/data",(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+    return res.status(200).json({
+        message: "Data received successfully",
+        data: req.body
+    })
+})
+
+
+app.get("/data", (req,res)=>{
+    return res.status(200).json({
+        message: "Data received successfully",
+        count: notes.length,
+        data: notes
+    })
+})
+
+app.delete("data/:index", (req,res)=>{
+    const {index} = req.params;
+    const note = notes.splice(index, 1);
+    return res.status(200).json({
+        message: "Data deleted successfully",
+        data: note
+    })
+})
+
+
+app.get("/", (req,res)=>{
+    res.send(notes);
+})
+
 export default app; 
